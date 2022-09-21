78288e791fa3a03cff7a4470217771eab053e214
Vitaliy Shulik <shulikvitalya@gmail.com>
2022-09-21 10:54:39 +0600

    add MIT license and update solidity version

--- contracts/IDOFactory.sol
+++ contracts/IDOFactory.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-import "./IDOPool.sol";
+import "./IDOPoolSafeLaunchpad.sol";
 
 contract IDOFactory is Ownable {
     using SafeMath for uint256;
